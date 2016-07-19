57a1aa498a8998fb3a86da481937d9582ceab907
XiaoYiPeng <helloxyp@gmail.com>
2016-07-19 10:11:42 +0800

    添加oc项目

---
+++ BicycleBeijing-OC/BicycleBeijing-OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BicycleBeijing-OC
+//
+//  Created by XiaoYiPeng on 16/7/19.
+//  Copyright © 2016年 XiaoYiPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
