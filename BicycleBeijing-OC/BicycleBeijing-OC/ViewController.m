f288b918ef27ef991363962cb3e36ba9c2ea04f8
XiaoYiPeng <helloxyp@gmail.com>
2016-07-22 17:04:29 +0800

    添加自行车网点数据到地图上，用了点聚合（四叉树）。

--- BicycleBeijing-OC/BicycleBeijing-OC/ViewController.m
+++ BicycleBeijing-OC/BicycleBeijing-OC/ViewController.m
@@ -12,19 +12,192 @@
 #import "ImageUtilities.h"
 #import "DataManager.h"
 #import "Bicycle.h"
+#import "CoordinateQuadTree.h"
+#import "CustomCalloutView.h"
+#import "ClusterAnnotationView.h"
+#import "ClusterAnnotation.h"
 
 @interface ViewController ()<MAMapViewDelegate, AMapSearchDelegate>
 {
-    //地图
-    MAMapView *_mapView;
     //当前位置
     CLLocationCoordinate2D _currentCoordiate;
     AMapSearchAPI *_search;
 }
 
+/**
+ *  地图
+ */
+@property (nonatomic, strong) MAMapView *mapView;
+/**
+ *  四叉权
+ */
+@property (nonatomic, strong) CoordinateQuadTree* coordinateQuadTree;
+/**
+ *  自定义的calloutView
+ */
+@property (nonatomic, strong) CustomCalloutView *customCalloutView;
+/**
+ *  当前选中的POI
+ */
+@property (nonatomic, strong) NSMutableArray *selectedPoiArray;
+/**
+ *
+ */
+@property (nonatomic, assign) BOOL shouldRegionChangeReCalculate;
+
 @end
 
 @implementation ViewController
+
+/* 更新annotation. */
+- (void)updateMapViewAnnotationsWithAnnotations:(NSArray *)annotations
+{
+    /* 用户滑动时，保留仍然可用的标注，去除屏幕外标注，添加新增区域的标注 */
+    NSMutableSet *before = [NSMutableSet setWithArray:self.mapView.annotations];
+    [before removeObject:[self.mapView userLocation]];
+    NSSet *after = [NSSet setWithArray:annotations];
+
+    /* 保留仍然位于屏幕内的annotation. */
+    NSMutableSet *toKeep = [NSMutableSet setWithSet:before];
+    [toKeep intersectSet:after];
+
+    /* 需要添加的annotation. */
+    NSMutableSet *toAdd = [NSMutableSet setWithSet:after];
+    [toAdd minusSet:toKeep];
+
+    /* 删除位于屏幕外的annotation. */
+    NSMutableSet *toRemove = [NSMutableSet setWithSet:before];
+    [toRemove minusSet:after];
+
+    /* 更新. */
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.mapView addAnnotations:[toAdd allObjects]];
+        [self.mapView removeAnnotations:[toRemove allObjects]];
+    });
+}
+
+- (void)addAnnotationsToMapView:(MAMapView *)mapView
+{
+    @synchronized(self)
+    {
+        if (self.coordinateQuadTree.root == nil || !self.shouldRegionChangeReCalculate)
+        {
+            NSLog(@"tree is not ready.");
+            return;
+        }
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            /* 根据当前zoomLevel和zoomScale 进行annotation聚合. */
+            double zoomScale = self.mapView.bounds.size.width / self.mapView.visibleMapRect.size.width;
+
+            NSArray *annotations = [self.coordinateQuadTree clusteredAnnotationsWithinMapRect:mapView.visibleMapRect
+                                                                                withZoomScale:zoomScale
+                                                                                 andZoomLevel:mapView.zoomLevel];
+            /* 更新annotation. */
+            [self updateMapViewAnnotationsWithAnnotations:annotations];
+        });
+    }
+}
+
+#pragma mark - 按钮事件
+- (void)scaleZoom:(UIButton *)btn {
+    CGFloat zoomLevel = _mapView.zoomLevel;
+    switch (btn.tag) {
+        case 101:
+        {
+            zoomLevel = fminf(zoomLevel+1, 19);
+            [_mapView setZoomLevel:zoomLevel animated:YES];
+        }
+            break;
+
+        case 102:
+        {
+            zoomLevel = fmaxf(zoomLevel-1, 3);
+            [_mapView setZoomLevel:zoomLevel animated:YES];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)locationButtonClicked:(UIButton *)btn {
+    if (_currentCoordiate.latitude > 0) {
+        CLLocationCoordinate2D myCoordinate = _currentCoordiate;
+        MACoordinateRegion theRegion = MACoordinateRegionMake(myCoordinate, MACoordinateSpanMake(0.2, 0.2));
+        [_mapView setScrollEnabled:YES];
+        [_mapView setRegion:theRegion animated:YES];
+        [_mapView setZoomLevel:16.1 animated:NO];
+    }
+}
+
+
+#pragma mark - MAMapViewDelegate
+//定位成功返回
+-(void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation
+updatingLocation:(BOOL)updatingLocation
+{
+    if(updatingLocation)
+    {
+        //取出当前位置的坐标
+        DLog(@"latitude : %f,longitude: %f",userLocation.coordinate.latitude,userLocation.coordinate.longitude);
+        _currentCoordiate = userLocation.coordinate;
+    }
+}
+
+- (void)mapView:(MAMapView *)mapView regionDidChangeAnimated:(BOOL)animated
+{
+    DLog(@"地带变化");
+    [self addAnnotationsToMapView:self.mapView];
+}
+
+- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id<MAAnnotation>)annotation
+{
+    if ([annotation isKindOfClass:[ClusterAnnotation class]])
+    {
+        /* dequeue重用annotationView. */
+        static NSString *const AnnotatioViewReuseID = @"AnnotatioViewReuseID";
+
+        ClusterAnnotationView *annotationView = (ClusterAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:AnnotatioViewReuseID];
+
+        if (!annotationView)
+        {
+            annotationView = [[ClusterAnnotationView alloc] initWithAnnotation:annotation
+                                                               reuseIdentifier:AnnotatioViewReuseID];
+        }
+
+        /* 设置annotationView的属性. */
+        annotationView.annotation = annotation;
+        annotationView.count = [(ClusterAnnotation *)annotation count];
+
+        /* 不弹出原生annotation */
+        annotationView.canShowCallout = NO;
+
+        return annotationView;
+    }
+
+    return nil;
+}
+
+#pragma mark - AMapSearchDelegate
+//云图搜索回调
+- (void)onCloudSearchDone:(AMapCloudSearchBaseRequest *)request response:(AMapCloudPOISearchResponse *)response
+{
+    if(response.POIs.count == 0)
+    {
+        return;
+    }
+    for (AMapCloudPOI *poi in response.POIs) {
+        MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
+        pointAnnotation.coordinate = CLLocationCoordinate2DMake(poi.location.latitude, poi.location.longitude);
+        pointAnnotation.title = poi.name;
+        pointAnnotation.subtitle = poi.address;
+        [_mapView addAnnotation:pointAnnotation];
+    }
+
+}
+
+#pragma mark - Life Cycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -32,25 +205,32 @@
     //初始化检索对象
     _search = [[AMapSearchAPI alloc] init];
     _search.delegate = self;
+    self.coordinateQuadTree = [[CoordinateQuadTree alloc] init];
+    self.selectedPoiArray = [[NSMutableArray alloc] init];
+    self.customCalloutView = [[CustomCalloutView alloc] init];
 
     [self addMapView];
     [self addScaleButton];
     [self addLocationButton];
-//    [self addAnnotation];
+    [self addAnnotation];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
 }
 
 /**
  *  添加地图
  */
 - (void)addMapView {
-    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
-    _mapView.delegate = self;
-    _mapView.showsCompass = NO;
-    _mapView.scaleOrigin = CGPointMake(SCREEN_WIDTH - 150, SCREEN_HEIGHT - 30);
-    _mapView.showsUserLocation = YES;
-    [_mapView setUserTrackingMode: MAUserTrackingModeFollow animated:YES];
+    self.mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    self.mapView.delegate = self;
+    self.mapView.showsCompass = NO;
+    self.mapView.scaleOrigin = CGPointMake(SCREEN_WIDTH - 150, SCREEN_HEIGHT - 30);
+    self.mapView.showsUserLocation = YES;
+    [self.mapView setUserTrackingMode: MAUserTrackingModeFollow animated:YES];
 
-    [self.view addSubview:_mapView];
+    [self.view addSubview:self.mapView];
 }
 
 /**
@@ -114,136 +294,19 @@
  */
 - (void)addAnnotation {
     NSArray<Bicycle *> *bicycles = [DataManager getBicycles];
-    for (Bicycle *bicycle in bicycles) {
-        MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
-        pointAnnotation.coordinate = CLLocationCoordinate2DMake(bicycle.latitude, bicycle.longitude);
-        pointAnnotation.title = bicycle.name;
-        pointAnnotation.subtitle = bicycle.address;
-        [_mapView addAnnotation:pointAnnotation];
-    }
-}
 
+    self.shouldRegionChangeReCalculate = NO;
 
-#pragma mark - 按钮事件
-- (void)scaleZoom:(UIButton *)btn {
-    CGFloat zoomLevel = _mapView.zoomLevel;
-    switch (btn.tag) {
-        case 101:
-        {
-            zoomLevel = fminf(zoomLevel+1, 19);
-            [_mapView setZoomLevel:zoomLevel animated:YES];
-        }
-            break;
+    // 清理
+    [self.selectedPoiArray removeAllObjects];
+    [self.customCalloutView dismissCalloutView];
+    [self.mapView removeAnnotations:self.mapView.annotations];
 
-        case 102:
-        {
-            zoomLevel = fmaxf(zoomLevel-1, 3);
-            [_mapView setZoomLevel:zoomLevel animated:YES];
-        }
-            break;
-        default:
-            break;
-    }
-}
-
-- (void)locationButtonClicked:(UIButton *)btn {
-    if (_currentCoordiate.latitude > 0) {
-        CLLocationCoordinate2D myCoordinate = _currentCoordiate;
-        MACoordinateRegion theRegion = MACoordinateRegionMake(myCoordinate, MACoordinateSpanMake(0.2, 0.2));
-        [_mapView setScrollEnabled:YES];
-        [_mapView setRegion:theRegion animated:YES];
-        [_mapView setZoomLevel:16.1 animated:NO];
-    }
-}
-
-
-#pragma mark - MAMapViewDelegate
-//定位成功返回
--(void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation
-updatingLocation:(BOOL)updatingLocation
-{
-    if(updatingLocation)
-    {
-        //取出当前位置的坐标
-        DLog(@"latitude : %f,longitude: %f",userLocation.coordinate.latitude,userLocation.coordinate.longitude);
-        _currentCoordiate = userLocation.coordinate;
-    }
-}
-
-//定位失败返回
-- (void)mapView:(MAMapView *)mapView didFailToLocateUserWithError:(NSError *)error
-{
-    DLog(@"定位失败，请检查配置");
-}
-
-/**
- *  地图将要发生移动时调用此接口
-*/
-- (void)mapView:(MAMapView *)mapView mapWillMoveByUser:(BOOL)wasUserAction {
-    DLog(@"地图将要移动");
-}
-
-/**
- *  地图移动结束后调用此接口
-*/
-- (void)mapView:(MAMapView *)mapView mapDidMoveByUser:(BOOL)wasUserAction {
-    DLog(@"地图结束移动");
-}
-
-/**
- *  地图将要发生缩放时调用此接口
- */
-- (void)mapView:(MAMapView *)mapView mapWillZoomByUser:(BOOL)wasUserAction {
-    DLog(@"地图将要缩放");
-}
-
-/**
- *  地图缩放结束后调用此接口
- */
-- (void)mapView:(MAMapView *)mapView mapDidZoomByUser:(BOOL)wasUserAction {
-    DLog(@"地图结束缩放");
-}
-
-/**
- * @brief 地图开始加载
- */
-- (void)mapViewWillStartLoadingMap:(MAMapView *)mapView {
-    DLog(@"地图开始加载");
-}
-
-/**
- * @brief 地图加载成功
- */
-- (void)mapViewDidFinishLoadingMap:(MAMapView *)mapView {
-    DLog(@"地图加载成功");
-}
-
-//地图加载失败
-- (void)mapViewDidFailLoadingMap:(MAMapView *)mapView withError:(NSError *)error
-{
-    DLog(@"地图加载失败");
-}
-
-#pragma mark - AMapSearchDelegate
-//云图搜索回调
-- (void)onCloudSearchDone:(AMapCloudSearchBaseRequest *)request response:(AMapCloudPOISearchResponse *)response
-{
-    if(response.POIs.count == 0)
-    {
-        return;
-    }
-    for (AMapCloudPOI *poi in response.POIs) {
-        MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
-        pointAnnotation.coordinate = CLLocationCoordinate2DMake(poi.location.latitude, poi.location.longitude);
-        pointAnnotation.title = poi.name;
-        pointAnnotation.subtitle = poi.address;
-        [_mapView addAnnotation:pointAnnotation];
-    }
-
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        /* 建立四叉树. */
+        [self.coordinateQuadTree buildTreeWithPOIs:bicycles];
+        self.shouldRegionChangeReCalculate = YES;
+    });
 }
 
 @end
